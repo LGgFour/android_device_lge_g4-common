56c709d7e3cb68451ea95ccac78773842cd159b9
pavlaras <theodoroupaulos@gmail.com>
2020-07-19 10:50:01 +0300

    Revert "g4-common: gps: Do not build "android.hardware.gnss@1.0" implementation and service"
    This reverts commit 72169012efc2cf01c45eaa7b00aa9fdcb1458a5f.

--- g4.mk
+++ g4.mk
@@ -126,6 +126,8 @@ TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
 
 # GPS
 PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service \
     gps.msm8992 \
     quipc.conf
 
