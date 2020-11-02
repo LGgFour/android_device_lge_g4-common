193e82feb01cdd69e08cf9d483acb26e4b3279c5
steadfasterX <steadfasterX@gmail.com>
2020-11-02 12:47:59 +0100

    Revert "g4-common: AOSiP doesnt support LiveDisplay, so remove it"
    This reverts commit bf2aef9bd889c79ca6d873de9553d7375324e664.

--- g4.mk
+++ g4.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
 	VisualizationWallpapers \
 	librs_jni
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # MP-CTL
 PRODUCT_PACKAGES += \
     libqti-perfd \
