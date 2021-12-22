6202c3040fbf6150223f21641768c522c1e16fbd
Utkarsh Tiwari <DevUt@users.noreply.github.com>
2021-12-22 20:55:28 +0530

    g4-common: shim libc/bionic/pthread_internal.cpp so pthread_t errors are ignored
    This allows the stock camera blobs to work finally in Q and above.
    
    01-08 22:46:09.943 770 1093 F libc : invalid pthread_t 0xea776230 passed to pthread_join 01-08 22:46:09.943 770 1093 F libc : Fatal signal 6 (SIGABRT), code -1 (SI_QUEUE) in tid 1093
    (mm-qcamera-daem), pid 770 (mm-qcamera-daem) 01-08 22:46:09.952 770 1092 F libc : invalid pthread_t 0xea372230 passed to pthread_join# Conflicts: 01-08 22:46:10.003 1096 1096 F DEBUG : *** *** ***
    *** *** *** *** *** *** *** *** *** *** *** *** ***# g4.mk 01-08 22:46:10.003 1096 1096 F DEBUG : Build fingerprint: 'lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys'#
    libshims/Android.mk 01-08 22:46:10.003 1096 1096 F DEBUG : Revision: '0'# 01-08 22:46:10.004 1096 1096 F DEBUG : ABI: 'arm'# It looks like you may be committing a cherry-pick. 01-08 22:46:10.005
    1096 1096 F DEBUG : Timestamp: 2015-01-08 22:46:10+0000# If this is not correct, please remove the file 01-08 22:46:10.005 1096 1096 F DEBUG : pid: 770, tid: 1093, name: mm-qcamera-daem >>>
    /vendor/bin/mm-qcamera-daemon <<<# .git/CHERRY_PICK_HEAD 01-08 22:46:10.006 1096 1096 F DEBUG : uid: 1006# and try again. 01-08 22:46:10.006 1096 1096 F DEBUG : signal 6 (SIGABRT), code -1
    (SI_QUEUE), fault addr -------- 01-08 22:46:10.007 1096 1096 F DEBUG : Abort message: 'invalid pthread_t 0xea776230 passed to pthread_join' 01-08 22:46:10.007 1096 1096 F DEBUG : r0 00000000 r1
    00000445 r2 00000006 r3 ed31a018# Please enter the commit message for your changes. Lines starting 01-08 22:46:10.007 1096 1096 F DEBUG : r4 ed31a02c r5 ed31a010 r6 00000302 r7 0000016b# with '#'
    will be ignored, and an empty message aborts the commit. 01-08 22:46:10.008 1096 1096 F DEBUG : r8 ed31a028 r9 ed31a018 r10 ed31a048 r11 ed31a038#
    01-08 22:46:10.008  1096  1096 F DEBUG   :     ip  00000445  sp  ed319fe8  lr  f2c26263  pc  f2c26276

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -215,7 +215,8 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libmmcamera_stillmore_lib.so|/system/lib/libshim_cameraclient.so \
     /system/vendor/lib/hw/camera.msm8992.so|/system/vendor/lib/libfence_shim.so \
     /system/vendor/lib64/lib-rtpcore.so|/system/vendor/lib64/ims_rtp_shim.so \
-    /system/vendor/bin/slim_daemon|/system/vendor/lib64/slim_shim.so
+    /system/vendor/bin/slim_daemon|/system/vendor/lib64/slim_shim.so \
+    /system/lib/libc.so|/system/vendor/lib/libpthread_shim.so
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
