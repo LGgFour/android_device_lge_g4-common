b107d4d6a26d7e1aa60d49ff2c613c8466f74ef9
steadfasterX <steadfasterX@users.noreply.github.com>
2022-08-11 11:26:55 +0200

    g4-common: add new vendor patch flag

--- system_prop.mk
+++ system_prop.mk
@@ -256,7 +256,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2017-07-01
+    ro.vendor.build.security_patch=2017-07-01 \
+    ro.lineage.build.vendor_security_patch=2017-07-01
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
