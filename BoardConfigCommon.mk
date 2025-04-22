0315448d513a4316e927d9514026876984bf23a3
steadfasterX <steadfasterX@users.noreply.github.com>
2025-04-22 11:12:12 +0200

    mk: enable AXP.OS boardconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -258,3 +258,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/lge/g4-common/BoardConfigVendor.mk
+
+# even though we include vendor/axp/config/common.mk we need to include AXP's own BoardConfig
+# (after the above definitions & includes), too so we we can make use of the conditions within
+include vendor/axp/BoardConfigVendor.mk
